020e51630a40235615b4abcc4ca989f0ddc777fd
Biz Growth Marketers <business.growthmarketers@gmail.com>
2024-10-12 03:59:34 -0700

    added meta, header, modal, scroll-btn (#17)
    Co-authored-by: Percival Aceron <perci.aceron@gmail.com>

--- js/whyads.js
+++ js/whyads.js
@@ -1,9 +1,12 @@
-const swiper = new Swiper(".pagination", {
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }
-  });
-  
+const swiper = new Swiper('.pagination', {
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
